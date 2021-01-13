c9e0cb729fe4ce829b798cd3671148be5c8ccbc5
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-13 20:27:39 +0100

    Implement getCollidedNodePath function.

--- Panda project/src/gameFunctions.cpp
+++ Panda project/src/gameFunctions.cpp
@@ -341,12 +341,9 @@ namespace game {
 
 		if (!in_out_pattern) {
 			if (std::round(entry->get_into_node_path().get_parent().get_z()) <= std::round(entry->get_from_node_path().get_parent().get_z())) {
-				playerOnGround = true;
-			} else {
-				playerOnGround = false;
+				NodePath intoObject = entry->get_into_node_path().get_parent();
+				entry->get_from_node_path().get_parent().set_pos(intoObject.get_x() + 2, intoObject.get_y(), intoObject.get_z() + 2);
 			}
-		} else {
-			playerOnGround = false;
 		}
 	}
 }
