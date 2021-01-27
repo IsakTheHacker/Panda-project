2c941b30557df4c893a689f2fa6827519f1c815b
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-28 00:03:55 +0100

    Make functions in cppExtension.cpp more efficient by passing parameters by const reference.

--- Panda project/src/cppExtension.cpp
+++ Panda project/src/cppExtension.cpp
@@ -4,13 +4,13 @@ int pass() {
 	return 0;
 }
 
-char stoc(std::string input) {
+char stoc(const std::string& input) {
 	char output = input[0];
 	return output;
 }
 
 namespace std {
-	string to_string(string input) {
+	string to_string(const string& input) {
 		return input;
 	}
 }
