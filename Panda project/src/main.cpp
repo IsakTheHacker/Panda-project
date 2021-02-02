bb09b68267cfafc577dc154377a5ed659b180aca
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-02 21:45:17 +0100

    Remove unnecessary output to the console.

--- Panda project/src/main.cpp
+++ Panda project/src/main.cpp
@@ -234,9 +234,6 @@ int main(int argc, char* argv[]) {
 	NodePath textNP = window->get_aspect_2d().attach_new_node(textNode);
 	textNP.set_scale(0.07);
 	textNP.set_pos((0 - textNode->get_width() * 0.07) / 2, 0, (0 - textNode->get_height() * 0.07 * 0.64) / 2);
-	game::timingInfoOut(defbutNP.get_x());
-	game::timingInfoOut(0 - textNode->get_width() * 0.07*1.5);
-	game::timingInfoOut(textNode->get_width());
 
 	//Set up frame rate meter
 	if (!std::stoi(options["hide_fps"])) {
