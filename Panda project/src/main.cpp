b23a2e2e1867c31564bc6c8af9958790cf20222f
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-05 21:37:46 +0100

    Add leaves block to hotbar.

--- Panda project/src/main.cpp
+++ Panda project/src/main.cpp
@@ -373,11 +373,12 @@ int main(int argc, char* argv[]) {
 				"data/assets/blockproperties/stone.blockproperties|" <<
 				std::endl;
 			createProfile.close();
-			playerHandInventory.resize(4);
+			playerHandInventory.resize(5);
 			playerHandInventory.setItem(0, game::item("data/assets/blockproperties/grass.blockproperties", 1));
 			playerHandInventory.setItem(1, game::item("data/assets/blockproperties/rotational-complex.blockproperties", 1));
 			playerHandInventory.setItem(2, game::item("data/assets/blockproperties/log.blockproperties", 1));
 			playerHandInventory.setItem(3, game::item("data/assets/blockproperties/stone.blockproperties", 1));
+			playerHandInventory.setItem(4, game::item("data/assets/blockproperties/leaves.blockproperties", 1));
 		} else {
 			std::map<std::string, std::string> vector;
 			std::string line;
